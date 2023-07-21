6bd2fcc9c960776830fdb3a333fb35aa8afe71cc
henryk1229 <henryk1229@gmail.com>
2023-07-21 10:10:25 -0400

    fix: initialize first word in App

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -3,17 +3,28 @@ import styles from './app.module.css';
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import Header from './header';
+import SpringBoard from './spring-board';
 
-import Board from './board';
+const mockInitialWordFetch = () => {
+  const submittedWords = localStorage.getItem('submittedWords');
+  if (!submittedWords) {
+    // words are an array of letters
+    const dummyWord = ['m', 'e', 'r', 'i', 't'];
+    // submitted words is an array of word arrays
+    localStorage.setItem('submittedWords', JSON.stringify([dummyWord]));
+  }
+};
 
 export function App() {
+  // TODO - generate + serve first word
+  mockInitialWordFetch();
+
   return (
     <>
       <CssBaseline />
       <Header />
-
       <Container className="board" maxWidth="lg">
-        <Board />
+        <SpringBoard />
       </Container>
     </>
   );
