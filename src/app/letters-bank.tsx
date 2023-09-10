7b2730fb086ea05811d7e06baffa73152c958e65
henryk1229 <henryk1229@gmail.com>
2023-09-10 11:09:07 -0400

    feat: keyboard layout

--- src/app/letters-bank.tsx
+++ src/app/letters-bank.tsx
@@ -3,7 +3,6 @@ import { useMemo } from 'react';
 import LettersBankTile from './letters-bank-tile';
 
 const LettersBankContainer = styled('div', {
-  height: '360px',
   width: '320px',
   display: 'flex',
   flexDirection: 'column',
@@ -15,65 +14,36 @@ interface Props {
   usedLetters: string[];
 }
 
-const makeConsonantsMatrix = () => {
-  return [
-    ['b', 'c', 'd', 'f', 'g'],
-    ['h', 'j', 'k', 'l', 'm'],
-    ['n', 'p', 'q', 'r', 's'],
-    ['t', 'v', 'w'],
-    ['x', 'y', 'z'],
-  ];
-};
-
-const makeVowelsMatrix = () => {
-  return ['a', 'e', 'i', 'o', 'u'];
+// an array of array of letters grouped by standard keyboard layout
+const makeLettersMatrix = () => {
+  return ['qwertyuiop'.split(''), 'asdfghjkl'.split(''), 'zxcvbnm'.split('')];
 };
 
 const LettersBank: React.FC<Props> = ({ usedLetters }) => {
-  const groupedConsonants = useMemo(() => makeConsonantsMatrix(), []);
-  const vowels = useMemo(() => makeVowelsMatrix(), []);
-  const availableConsonants = groupedConsonants.map((group) =>
-    group.filter((letter) => !usedLetters.includes(letter))
-  );
-  const availableVowels = vowels.filter(
-    (vowel) => !usedLetters.includes(vowel)
-  );
+  const groupedLetters = useMemo(() => makeLettersMatrix(), []);
+
   return (
     <LettersBankContainer>
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          margin: '8px',
-          width: '180px',
-        }}
-      >
-        <div style={{ display: 'flex', margin: '2px' }}>
-          {availableVowels.map((vowel) => (
-            <LettersBankTile
-              key={`letter-${vowel}`}
-              letter={vowel}
-              isUsedLetter={usedLetters.includes(vowel)}
-            />
-          ))}
-        </div>
-        {availableConsonants.map((consonants) => {
-          return (
-            <div
-              style={{ display: 'flex', margin: '2px' }}
-              key={`array-${consonants[0]}`}
-            >
-              {consonants.map((letter) => (
-                <LettersBankTile
-                  key={`letter-${letter}`}
-                  letter={letter}
-                  isUsedLetter={usedLetters.includes(letter)}
-                />
-              ))}
-            </div>
-          );
-        })}
-      </div>
+      {groupedLetters.map((letters) => {
+        return (
+          <div
+            style={{
+              display: 'flex',
+              margin: '2px',
+              justifyContent: 'center',
+            }}
+            key={`array-${letters[0]}`}
+          >
+            {letters.map((letter) => (
+              <LettersBankTile
+                key={`letter-${letter}`}
+                letter={letter}
+                isUsedLetter={usedLetters.includes(letter)}
+              />
+            ))}
+          </div>
+        );
+      })}
     </LettersBankContainer>
   );
 };
