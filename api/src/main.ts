b48f5480e062a2bc579a337f1d4b111ef8efa6d3
henryk1229 <henryk1229@gmail.com>
2023-09-21 12:44:29 -0400

    feat: initial db work for local dev

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,20 +1,41 @@
 import express from 'express';
-import { wordGenerator } from './word-generator.js';
+import { wordGenerator } from './word-generator';
 import cors from 'cors';
-import { spellcheckWord } from './spellchecker.js';
+import config from './config';
+import { spellcheckWord } from './spellchecker';
+import { query } from './db';
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: (_, callback) => {
+      callback(null, config.allowedOrigins);
+    },
+    credentials: true,
+  })
+);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/', (_req, res) => {
-  const randomWord = wordGenerator();
-  res.send({ randomWord });
+// TODO - handle fetching errors
+app.get('/', async (_req, res) => {
+  const current_date = new Date();
+  const yyyyMmDd = current_date.toISOString().split('T')[0];
+  // salads.date = postgres current_date fn
+  const formatted = `${yyyyMmDd} 00:00:00`;
+  const { rows } = await query({
+    text: 'select * from salads where salads.date = $1',
+    params: [formatted],
+  });
+  // while in dev mode, allow multiple games per day
+  const latestSalad = rows[rows.length - 1];
+  if (latestSalad) {
+    res.send({ salad: latestSalad });
+  }
 });
 
 app.post('/spellcheck', (req, res) => {
