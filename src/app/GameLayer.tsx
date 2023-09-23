11041f027be166efc558cca90bf039fffee062c5
henryk1229 <henryk1229@gmail.com>
2023-09-23 17:24:15 -0400

    feat: raise localStorage logic to game layer, add restart game, playedWords state

--- src/app/GameLayer.tsx
+++ src/app/GameLayer.tsx
@@ -1,10 +1,9 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import GameBoard from './GameBoard';
 import { useState } from 'react';
 import EndGameModal from './EndGameModal';
 import { DailySalad } from './app';
 
-// // TODO - handle local storage here?
+// // TODO - set salad details in local storage for streak tracking?
 // const setSaladInStorage = (dailySalad: DailySalad | null) => {
 //   if (dailySalad) {
 //     // const current_date = new Date();
@@ -22,13 +21,51 @@ interface Props {
 
 const GameLayer: React.FC<Props> = ({ dailySalad }) => {
   // handle end game logic
-  const [shouldEndGame, setShouldEndGame] = useState<boolean>(true);
+  const [shouldEndGame, setShouldEndGame] = useState<boolean>(false);
 
+  // create rootWord[] from daily initialWord
   const { date, saladNumber, par, initialWord } = dailySalad;
+  const rootWord = initialWord.split('');
+
+  // track stored words in localStorage
+  const storedWords = localStorage.getItem('submittedWords') ?? '[]';
+  const submittedWords: string[][] | [] = JSON.parse(storedWords);
+
+  const [playedWords, setPlayedWords] = useState<string[][]>(() =>
+    submittedWords.length > 0 ? [rootWord, ...submittedWords] : [rootWord]
+  );
+
+  // track attempts in lS
+  const storedAttempts = localStorage.getItem('attempts') ?? '1';
+  const attempts = parseInt(storedAttempts, 10);
+
+  const restartGame = () => {
+    // tally restart
+    const newAttempts = attempts + 1;
+    localStorage.setItem('attempts', newAttempts.toString());
+    // clear submitted words
+    localStorage.removeItem('submittedWords');
+    // trigger refresh
+    setPlayedWords([rootWord]);
+  };
+
+  // this fn sets word in local storage, and set played word state
+  const playNewWord = (newWord: string[]) => {
+    const stringified = JSON.stringify([...submittedWords, newWord]);
+    localStorage.setItem('submittedWords', stringified);
+    setPlayedWords([...playedWords, newWord]);
+  };
 
   return (
     <>
-      <GameBoard dailySalad={dailySalad} setShouldEndGame={setShouldEndGame} />
+      <GameBoard
+        dailySalad={dailySalad}
+        playedWords={playedWords}
+        attempts={attempts}
+        playNewWord={playNewWord}
+        restartGame={restartGame}
+        setShouldEndGame={setShouldEndGame}
+      />
       <EndGameModal
         stats={{
           date,
@@ -36,7 +73,7 @@ const GameLayer: React.FC<Props> = ({ dailySalad }) => {
           par,
           initialWord,
           // TODO
-          attempts: 0,
+          attempts,
           ranking: 'Good',
         }}
         open={shouldEndGame}
