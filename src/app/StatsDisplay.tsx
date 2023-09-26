0c81524b12dd0d35776682ca12ffd7ef8b8dff6c
henryk1229 <henryk1229@gmail.com>
2023-09-26 18:39:57 -0400

    chore: clean up stats display

--- src/app/StatsDisplay.tsx
+++ src/app/StatsDisplay.tsx
@@ -4,13 +4,13 @@ import LettersBankTile from './letters-bank-tile';
 const DisplayContainer = styled('div', {
   width: '320px',
   display: 'flex',
-  flexDirection: 'column',
   alignItems: 'center',
   justifyContent: 'space-evenly',
 });
 
 const DisplayContent = styled('div', {
   display: 'flex',
+  flexDirection: 'column',
   fontFamily: 'Helvetica',
   fontWeight: 'bold',
 });
@@ -24,80 +24,31 @@ const StatsDisplay: React.FC<Props> = ({ par, attempts }) => {
   return (
     <DisplayContainer>
       <DisplayContent>
-        <div>
-          <div
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
-            <div
-              style={{
-                display: 'flex',
-                margin: '8px',
-              }}
-            >
-              {'STATS'.split('').map((letter, idx) => (
-                <LettersBankTile
-                  key={`letter-${letter}-${idx}`}
-                  letter={letter}
-                  isUsedLetter={false}
-                />
-              ))}
-            </div>
-            <div
-              style={{
-                margin: '4px',
-              }}
-            >
-              Attempts: {attempts}
-            </div>
-            <div
-              style={{
-                margin: '4px',
-              }}
-            >
-              Par: {par}
-            </div>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <div style={{ display: 'flex' }}>
+            {'STATS'.split('').map((letter, idx) => (
+              <LettersBankTile
+                key={`letter-${letter}-${idx}`}
+                letter={letter}
+                isUsedLetter={false}
+              />
+            ))}
           </div>
-          <div
-            style={{
-              display: 'flex',
-              justifyContent: 'space-evenly',
-              margin: '4px',
-            }}
-          >
-            <div
-              style={{
-                margin: '4px',
-              }}
-            >
-              Good: {par - 1}
-            </div>
-            <div
-              style={{
-                margin: '4px',
-              }}
-            >
-              Great: {par - 2}
-            </div>
-          </div>
-          <div
-            style={{
-              display: 'flex',
-              justifyContent: 'space-evenly',
-              margin: '2px',
-            }}
-          >
-            <div
-              style={{
-                margin: '4px',
-              }}
-            >
-              Genius: {par - 4}
-            </div>
-          </div>
+          <div style={{ margin: '4px' }}>Attempts: {attempts}</div>
+          <div style={{ margin: '4px' }}>Par: {par}</div>
+        </div>
+        <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+          <div style={{ margin: '4px' }}>Good: {par - 1}</div>
+          <div style={{ margin: '4px' }}>Great: {par - 2}</div>
+        </div>
+        <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+          <div style={{ margin: '4px' }}>Genius: {par - 4}</div>
         </div>
       </DisplayContent>
     </DisplayContainer>
