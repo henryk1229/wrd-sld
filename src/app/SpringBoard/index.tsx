cd03edb87572bdd27d7bc5e568fd143679e13ab3
henryk1229 <henryk1229@gmail.com>
2023-10-17 16:24:10 -0400

    feat: wip, initial wordsGrid

--- src/app/SpringBoard/index.tsx
+++ src/app/SpringBoard/index.tsx
@@ -1,34 +1,66 @@
 import { styled } from '@stitches/react';
-import { animated } from '@react-spring/web';
 import WordMatrix from './WordMatrix';
+import Tile from './Tile';
 
 const SpringBoardContainer = styled('div', {
   height: '360px',
   width: '360px',
   display: 'flex',
+  flexDirection: 'column',
   alignItems: 'center',
   justifyContent: 'center',
-});
-
-const SB = styled(animated.div, {
-  borderRadius: '3px',
-  padding: 4,
-  margin: 4,
 });
 
 interface Props {
   playedWords: string[][];
 }
 
+// TODO - do while loop?
+const makeWordsGrid = (playedWords: string[][]): string[][] => {
+  const wordsGrid = ['', '', '', ''];
+  return wordsGrid.map((position, idx) => {
+    if (playedWords[idx]) {
+      return playedWords[idx];
+    }
+    const precedingWord = playedWords[idx - 1];
+    const firstLetter = precedingWord
+      ? precedingWord[precedingWord.length - 1]
+      : '';
+    // TODO - playedWords or idx here?
+    const lastLetter = playedWords.length === 3 ? playedWords[0][0] : '';
+    return [firstLetter, '', '', '', lastLetter];
+  });
+};
+
 // This component rotates board when a word is submitted
-const SpringBoard: React.FC<Props> = (props) => {
-  const { playedWords } = props;
+const SpringBoard: React.FC<Props> = ({ playedWords }) => {
+  const wordsGrid = makeWordsGrid(playedWords);
 
   return (
     <SpringBoardContainer>
-      <SB>
-        <WordMatrix playedWords={playedWords} />
-      </SB>
+      {wordsGrid.map((word: string[], wordIdx: number) => {
+        return (
+          <div style={{ display: 'flex' }} key={wordIdx}>
+            {word.map((letter, letterIdx) => {
+              // first and last letters in array will be right and left bounds of board
+              const isBorderTile =
+                [0, 4].includes(letterIdx) &&
+                !word[1] &&
+                !!playedWords[wordIdx - 1] &&
+                !!letter;
+              const isAnchorTile = [0, 4].includes(letterIdx);
+              return (
+                <Tile
+                  key={letterIdx}
+                  letter={letter}
+                  isBorderTile={isBorderTile}
+                  isAnchorTile={isAnchorTile}
+                />
+              );
+            })}
+          </div>
+        );
+      })}
     </SpringBoardContainer>
   );
 };
