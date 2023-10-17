43cd38b2b2c01a7282a271794fb6879e1d38f21a
henryk1229 <henryk1229@gmail.com>
2023-10-17 13:49:41 -0400

    feat: improve styling

--- src/app/HowToPlayModal.tsx
+++ src/app/HowToPlayModal.tsx
@@ -13,7 +13,7 @@ const ModalHeader = styled('h3', {
 const ModalSubHeader = styled('div', {
   display: 'flex',
   flexDirection: 'column',
-  margin: '16px',
+  margin: '16px 16px 0px',
   color: '#217C7E',
   fontSize: '18px',
   fontWeight: 600,
@@ -22,8 +22,7 @@ const ModalSubHeader = styled('div', {
 const ModalContent = styled('div', {
   display: 'flex',
   flexDirection: 'column',
-  margin: '8px 16px',
-  // fontWeight: 500,
+  margin: '8px',
   fontSize: '16px',
 });
 
@@ -51,11 +50,10 @@ const HowToPlayModal: React.FC<Props> = ({ open, onClose }) => {
       aria-describedby="modal-indicating-game-over"
       styles={{
         modal: {
-          width: '48%',
+          width: '40%',
           borderRadius: '3px',
           backgroundColor: '#F3EFE0',
           fontFamily: 'Helvetica',
-          // fontWeight: 500,
           padding: '32px',
         },
       }}
@@ -68,58 +66,40 @@ const HowToPlayModal: React.FC<Props> = ({ open, onClose }) => {
       </ModalHeader>
       <ModalSubHeader>Complete the WordSalad in four words</ModalSubHeader>
       <ModalContent>
-        <li style={{ margin: '4px' }}>
-          The first word has been chosen for you
-        </li>
-        <li style={{ margin: '4px' }}>
-          The next word must start with the last letter of the preceding word
-        </li>
-        <li style={{ margin: '4px' }}>
-          The last word ends with the last letter of the first word
-        </li>
-        <li style={{ margin: '4px' }}>
-          Each letter can be used only{' '}
-          <span style={{ fontStyle: 'italic' }}>once</span> per WordSalad
-        </li>
-        <li style={{ margin: '4px' }}>Each word must be five letters</li>
+        <ul style={{ padding: '0px 0px 0px 16px', margin: '0px' }}>
+          <li style={{ margin: '8px' }}>
+            The first word has been chosen for you
+          </li>
+          <li style={{ margin: '8px' }}>
+            Each word must start with the last letter of the preceding word
+          </li>
+          <li style={{ margin: '8px' }}>
+            The last word ends with the first letter of the first word
+          </li>
+          <li style={{ margin: '8px' }}>
+            Each letter can be used only{' '}
+            <span style={{ fontWeight: 600 }}>once</span> per WordSalad
+          </li>
+          <li style={{ margin: '8px' }}>Each word must be five letters</li>
+        </ul>
       </ModalContent>
       <ModalSubHeader>Examples</ModalSubHeader>
       <ModalContent>
-        <li style={{ margin: '4px', fontWeight: 500 }}>
-          "
-          <span style={{ textDecoration: 'underline', fontWeight: 600 }}>
-            a
-          </span>
-          bcd
-          <span style={{ textDecoration: 'underline', fontWeight: 600 }}>
-            e
-          </span>
-          ", "
-          <span style={{ textDecoration: 'underline', fontWeight: 600 }}>
-            e
-          </span>
-          fgh
-          <span style={{ textDecoration: 'underline', fontWeight: 600 }}>
-            i
-          </span>
-          ", "
-          <span style={{ textDecoration: 'underline', fontWeight: 600 }}>
-            i
-          </span>
-          jkl
-          <span style={{ textDecoration: 'underline', fontWeight: 600 }}>
-            m
-          </span>
-          ", "
-          <span style={{ textDecoration: 'underline', fontWeight: 600 }}>
-            m
-          </span>
-          lno
-          <span style={{ textDecoration: 'underline', fontWeight: 600 }}>
-            a
-          </span>
-          "
-        </li>
+        <ul style={{ padding: '0px 0px 0px 16px', margin: '0px' }}>
+          <li style={{ margin: '8px', fontWeight: 600, letterSpacing: '2px' }}>
+            <span style={{ textDecoration: 'underline' }}>a</span>bcd
+            <span style={{ textDecoration: 'underline' }}>e</span>,{' '}
+            <span style={{ textDecoration: 'underline' }}>e</span>
+            fgh
+            <span style={{ textDecoration: 'underline' }}>i</span>,{' '}
+            <span style={{ textDecoration: 'underline' }}>i</span>
+            jkl
+            <span style={{ textDecoration: 'underline' }}>m</span>,{' '}
+            <span style={{ textDecoration: 'underline' }}>m</span>
+            lno
+            <span style={{ textDecoration: 'underline' }}>a</span>
+          </li>
+        </ul>
       </ModalContent>
     </Modal>
   );
