a5629466aa0f150f6a0e7851611af24ebf26ffac
henryk1229 <henryk1229@gmail.com>
2023-10-18 20:29:52 -0400

    feat: add HTP modal to stats display

--- src/app/StatsDisplay/index.tsx
+++ src/app/StatsDisplay/index.tsx
@@ -1,6 +1,8 @@
 import { styled } from '@stitches/react';
 import { useState } from 'react';
 import StatsModal, { Stats } from './StatsModal';
+import HelpButton from '../HelpButton';
+import HowToPlayModal from '../HowToPlayModal';
 
 const DisplayContainer = styled('div', {
   width: '320px',
@@ -14,7 +16,7 @@ const DisplayContent = styled('div', {
   flexDirection: 'column',
   fontFamily: 'Helvetica',
   fontWeight: 400,
-  color: '#217C7E',
+  justifyContent: 'left',
 });
 
 interface Props {
@@ -23,23 +25,39 @@ interface Props {
 
 const StatsDisplay: React.FC<Props> = ({ stats }) => {
   const { attempts, ranking, par } = stats;
-  const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [statsModalOpen, setStatsModalOpen] = useState<boolean>(false);
+  const [howToPlayModalOpen, setHTPModalOpen] = useState<boolean>(false);
+
   return (
     <DisplayContainer>
       <DisplayContent>
-        <div style={{ margin: '4px' }}>Attempts: {attempts}</div>
-        <div style={{ margin: '4px' }}>Par: {par}</div>
         <div
           style={{ margin: '4px', cursor: 'pointer' }}
-          onClick={() => setModalOpen(true)}
+          onClick={() => setStatsModalOpen(true)}
         >
-          Current Rank: {ranking}
+          Current Rank:{' '}
+          <span
+            style={{
+              color: '#217C7E',
+            }}
+          >
+            {ranking}
+          </span>
+        </div>
+        <div style={{ margin: '4px' }}>Par: {par}</div>
+        <div style={{ margin: '4px' }}>Attempts: {attempts}</div>
+        <div style={{ margin: '4px 0px' }}>
+          <HelpButton onClick={() => setHTPModalOpen(true)} />
         </div>
       </DisplayContent>
       <StatsModal
         stats={stats}
-        open={modalOpen}
-        onClose={() => setModalOpen(false)}
+        open={statsModalOpen}
+        onClose={() => setStatsModalOpen(false)}
+      />
+      <HowToPlayModal
+        open={howToPlayModalOpen}
+        onClose={() => setHTPModalOpen(false)}
       />
     </DisplayContainer>
   );
