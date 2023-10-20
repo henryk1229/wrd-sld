0ada5322eadb48218ae12a8f128d4b46e6f33659
henryk1229 <henryk1229@gmail.com>
2023-10-20 12:34:04 -0400

    feat: initial animated word grid

--- src/app/WordsGrid/index.tsx
+++ src/app/WordsGrid/index.tsx
@@ -1,6 +1,7 @@
 import { styled } from '@stitches/react';
 import Tile from './Tile';
 import GuideLine from './GuideLine';
+import { useTrail } from '@react-spring/web';
 
 const WordsGridContainer = styled('div', {
   height: '360px',
@@ -51,6 +52,7 @@ const WordsGrid: React.FC<Props> = ({ playedWords }) => (
   <WordsGridContainer>
     {makeWordsGrid(playedWords).map((word: string[], wordIdx: number) => {
       const isPendingWord = !!word[0] && !word[1];
+      const isLastPlayedWord = wordIdx === playedWords.length - 1;
       return (
         <div
           style={{
@@ -60,18 +62,18 @@ const WordsGrid: React.FC<Props> = ({ playedWords }) => (
           key={wordIdx}
         >
           <TileWrapper>
-            {word.map((letter, letterIdx) => {
-              // first and last letters in array will be right and left bounds of board
-              const isAnchorTile = [0, 4].includes(letterIdx);
-              return (
+            {isLastPlayedWord ? (
+              <AnimatedWord word={word} />
+            ) : (
+              word.map((letter, letterIdx) => (
                 <Tile
                   key={letterIdx}
                   letter={letter}
                   isPendingWord={isPendingWord}
-                  isAnchorTile={isAnchorTile}
+                  isAnchorTile={[0, 4].includes(letterIdx)}
                 />
-              );
-            })}
+              ))
+            )}
           </TileWrapper>
           {wordIdx !== 3 && <GuideLine />}
         </div>
@@ -79,5 +81,33 @@ const WordsGrid: React.FC<Props> = ({ playedWords }) => (
     })}
   </WordsGridContainer>
 );
+
+interface WordProps {
+  word: string[];
+}
+const AnimatedWord: React.FC<WordProps> = ({ word }) => {
+  const trails = useTrail(word.length, {
+    from: { transform: 'scale(0.7)' },
+    to: {
+      transform: 'scale(1)',
+    },
+    config: {
+      duration: 100,
+    },
+  });
+  return trails.map((trail, idx) => {
+    // first and last letters in array will be right and left bounds of board
+    const isAnchorTile = [0, 4].includes(idx);
+    return (
+      <Tile
+        key={idx}
+        letter={word[idx]}
+        isPendingWord={false}
+        isAnchorTile={isAnchorTile}
+        spring={trail}
+      />
+    );
+  });
+};
 
 export default WordsGrid;
