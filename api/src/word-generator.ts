d3becddc8234fcd98359a19f87a56864d2184a39
henryk1229 <henryk1229@gmail.com>
2023-11-17 14:57:40 -0500

    feat: add endpoint, helper fns

--- api/src/word-generator.ts
+++ api/src/word-generator.ts
@@ -2,16 +2,16 @@
 const randomWords = require('better-random-words');
 
 // generates a random five letter word when called
-export const wordGenerator = () => {
-  let generatedWords = [];
-  let chosenWord = undefined;
+export const wordGenerator = (usedWords: string[]) => {
+  let generatedWords: string[] = [];
+  let chosenWord: string | undefined = undefined;
   while (!chosenWord) {
     for (const word of generatedWords) {
-      if (word.length === 5) {
+      if (word.length === 5 && !usedWords.includes(word)) {
         chosenWord = word;
       }
     }
     generatedWords = randomWords({ min: 5, max: 5 });
   }
-  return chosenWord.split('');
+  return chosenWord;
 };
