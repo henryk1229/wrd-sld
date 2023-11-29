8538391e63eeeae62b23a1569941ac87f6b0c771
henryk1229 <henryk1229@gmail.com>
2023-11-28 20:40:24 -0500

    feat: sleeker badges

--- src/app/WordsGrid/GuideLine.tsx
+++ src/app/WordsGrid/GuideLine.tsx
@@ -1,10 +1,10 @@
 const GuideLine: React.FC = () => {
   return (
-    <div style={{ height: '8px' }}>
+    <div style={{ height: '8px', width: '256px' }}>
       <svg xmlns="http://www.w3.org/2000/svg">
-        <line x1="50" y1="20" x2="50" y2="10" stroke="black" />
-        <line x1="50" y1="10" x2="250" y2="10" stroke="black" />
-        <line x1="250" y1="10" x2="250" y2="1" stroke="black" />
+        <line x1="28" y1="20" x2="28" y2="10" stroke="black" />
+        <line x1="28" y1="10" x2="228" y2="10" stroke="black" />
+        <line x1="228" y1="10" x2="228" y2="1" stroke="black" />
       </svg>
     </div>
   );
