a7c266cb0b742ee05c2f30a91ab03847c79f0073
henryk1229 <henryk1229@gmail.com>
2023-11-30 21:11:35 -0500

    feat: button classic

--- src/app/GameBoard/WordsGrid/index.tsx
+++ src/app/GameBoard/WordsGrid/index.tsx
@@ -23,10 +23,9 @@ const Badge = styled(animated.div, {
   color: 'black',
   height: '32px',
   width: '32px',
-  marginLeft: '8px',
+  marginLeft: '12px',
   border: '2px solid black',
   borderRadius: '50%',
-  boxShadow: '1px 1px #5C5C5C',
 });
 
 const BadgeContents = styled(animated.div, {
