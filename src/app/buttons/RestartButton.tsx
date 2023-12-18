70533e631e548be137fd7bc8f7472dfe73c068d8
henryk1229 <henryk1229@gmail.com>
2023-12-18 11:41:53 -0600

    feat: resize buttons

--- src/app/buttons/RestartButton.tsx
+++ src/app/buttons/RestartButton.tsx
@@ -35,8 +35,8 @@ const RestartButton: React.FC<Props> = ({ disabled, restartGame }) => (
 const RestartIcon = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width={32}
-    height={32}
+    width={26}
+    height={26}
     viewBox="0 0 24 24"
     fill="none"
     stroke="#9A3334"
